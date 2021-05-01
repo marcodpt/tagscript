9c98b625967cc2042287ce642bf5e0fe832385b7
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2021-05-01 18:22:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+Tags = Tags || {}
+
+var escapeHtml = function (unsafe) {
+  var t = typeof unsafe
+  return (t == 'number' || t == 'string' ? String(unsafe) : '')
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;")
+    .split("\n").join("&#13;&#10;")
+}
+
+var camelToKebab = function (string) {
+  return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
+    .toLowerCase()
+}
+
+var ident = function (pre, V, pos, max) {
+  var div = '\n  '
+  var a = '\n  '
+  var b = '\n'
+  if (max > 0) {
+    var len = V.reduce(function (l, v) {
+      return l + v.length
+    }, 0)
+    if (len < max) {
+      var div = ' '
+      var a = pre.substr(pre.length - 1) == '"' ? '' : ' '
+      var b = ''
+    }
+  }
+
+  return pre+a+V.map(function (v) {
+    return v.split('\n').join('\n  ')
+  }).join(div)+b+pos
+}
+
+var maxLine = 50
+
+var h = function (tagName, attributes, children) {
+  var X = Object.keys(attributes || {}).map(function (name) {
+    var v = attributes[name]
+    var n = camelToKebab(name)
+    if (v == null || v === false) {
+      return ''
+    } else if (v === true) {
+      return n
+    } else if (v instanceof Array && n == 'class') {
+      var X = v.filter(function (cls) {
+        return typeof cls == 'string' && cls.length
+      })
+      return X.length ? ident(n+'="', X, '"', maxLine) : ''
+    } else if (typeof v === 'object' && n == 'style') {
+      var X = Object.keys(v).map(function (css) {
+        if ((['boolean', 'number', 'string']).indexOf(typeof v[css]) != -1) {
+          var s = String(v[css])
+          return s.length ? camelToKebab(css)+': '+s+';' : ''
+        } else {
+          return ''
+        }
+      }).filter(function (v) {
+        return v.length > 0
+      })
+      return X.length ? ident(n+'="', X, '"', maxLine) : ''
+    } else if (typeof v == 'string' || typeof v == 'number'){
+      var s = String(v)
+      return n+'="'+s+'"'
+    } else {
+      return ''
+    }
+  }).filter(function (attr) {
+    return attr.length > 0
+  })
+
+  html = '<'+tagName
+  if (X.length) {
+    html = ident(html, X, '', maxLine)
+  }
+
+  if (children instanceof Array) {
+    children = children.filter(function (v) {
+      return typeof v == 'string' && v.length
+    })
+  }
+
+  if (children == null) {
+    html += '/>'
+  } else if (children instanceof Array && children.length) {
+    var div = '\n  '
+    html += ident('>', children, '</'+tagName+'>')
+  } else {
+    html += '>'+escapeHtml(children)+'</'+tagName+'>'
+  }
+
+  return html
+}
+
+var hSyntax = function (tagName, attributes, children) {
+  if (
+    children == null &&
+    (typeof attributes != 'object' || attributes instanceof Array)
+  ) {
+    return [tagName, {}, attributes]
+  } else {
+    return [tagName, attributes, children]
+  }
+}
+
+function run (tagName, attributes, children) {
+  var H = hSyntax(tagName, attributes, children)
+  if (typeof attributes == 'function') {
+    Tags[tagName] = attributes
+    return function (t, a, c) {
+      return run(t, a, c)
+    }
+  } else if (Tags[H[0]]) {
+    return Tags[H[0]](function (t, a, c) {
+      return run(t, a, c)
+    }, H[1], H[2])
+  } else {
+    return h(H[0], H[1], H[2])
+  }
+}
+
+const t = function (t, a, c) {
+  return run(t, a, c)
+}
+
+export t
---
+++ test.js
@@ -0,0 +1,370 @@
+import t from './index.js'
+
+QUnit.test("t", function (assert) {
+  assert.equal(t('a'), '<a/>')
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }), '<a href="www.google.com"/>')
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, "hello"), '<a href="www.google.com">hello</a>')
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, "<span>"), '<a href="www.google.com">&lt;span&gt;</a>')
+  assert.equal(t('a', {
+    href: 'www.google.com',
+    style: {
+      whiteSpace: 'pre-wrap'
+    }
+  }, "<span>"), `<a
+  href="www.google.com"
+  style="white-space: pre-wrap;"
+>&lt;span&gt;</a>`)
+  assert.equal(t('a', {
+    href: 'www.google.com',
+    style: {
+      whiteSpace: 'pre-wrap',
+      disabled: true,
+      active: null,
+      width: 0,
+      onBlur: false,
+      switch: {},
+      array: [],
+      king: ''
+    },
+    class: ['', '', ''],
+    disabled: true,
+    inactive: false,
+    user: null,
+    dataBind: 3.14,
+    array: ['x'],
+    object: {'y': 7},
+    king: ''
+  }, "<span>"), `<a
+  href="www.google.com"
+  style="
+    white-space: pre-wrap;
+    disabled: true;
+    width: 0;
+    on-blur: false;
+  "
+  disabled
+  data-bind="3.14"
+  king=""
+>&lt;span&gt;</a>`)
+  assert.equal(t('div', {}, [
+    t('a', {
+      href: 'www.google.com',
+      style: {
+        whiteSpace: 'pre-wrap'
+      }
+    }, [null, null])
+  ]), `<div>
+  <a
+    href="www.google.com"
+    style="white-space: pre-wrap;"
+  ></a>
+</div>`)
+  assert.equal(t('div', {}, [
+    t('a', {
+      href: 'www.google.com',
+      style: {
+        whiteSpace: 'pre-wrap'
+      }
+    }, "White Space\nShould not ident\nIn case of user data")
+  ]), `<div>
+  <a
+    href="www.google.com"
+    style="white-space: pre-wrap;"
+  >White Space&#13;&#10;Should not ident&#13;&#10;In case of user data</a>
+</div>`)
+  assert.equal(t('form', {
+    class: ['row', 'g-3']
+  }, [
+    t('div', {
+      class: 'col-md-6'
+    }, [
+      t('label', {
+        for: 'inputEmail4',
+        class: 'form-label'
+      }, 'Email'),
+      t('input', {
+        type: 'email',
+        class: 'form-control',
+        id: "inputEmail4"
+      })
+    ]),
+    t('div', {
+      class: 'col-md-6'
+    }, [
+      t('label', {
+        for: 'inputPassword4',
+        class: 'form-label'
+      }, 'Password'),
+      t('input', {
+        type: 'password',
+        class: 'form-control',
+        id: "inputPassword4"
+      })
+    ]),
+    t('div', {
+      class: 'col-12'
+    }, [
+      t('label', {
+        for: 'inputAddress',
+        class: 'form-label'
+      }, 'Address'),
+      t('input', {
+        type: 'text',
+        class: 'form-control',
+        id: "inputAddress",
+        placeholder: "1234 Main St"
+      })
+    ]),
+    t('div', {
+      class: 'col-12'
+    }, [
+      t('label', {
+        for: 'inputAddress2',
+        class: 'form-label'
+      }, 'Address 2'),
+      t('input', {
+        type: 'text',
+        class: 'form-control',
+        id: "inputAddress2",
+        placeholder: "Apartment, studio, or floor"
+      })
+    ]),
+    t('div', {
+      class: 'col-md-6'
+    }, [
+      t('label', {
+        for: 'inputCity',
+        class: 'form-label'
+      }, 'City'),
+      t('input', {
+        type: 'text',
+        class: 'form-control',
+        id: "inputCity"
+      })
+    ]),
+    t('div', {
+      class: 'col-md-4'
+    }, [
+      t('label', {
+        for: 'inputState',
+        class: 'form-label'
+      }, 'State'),
+      t('select', {
+        id: 'inputState',
+        class: 'form-select'
+      }, [
+        t('option', {
+          selected: true
+        }, 'Choose...'),
+        t('option', {}, '...')
+      ])
+    ]),
+    t('div', {
+      class: 'col-md-2'
+    }, [
+      t('label', {
+        for: 'inputZip',
+        class: 'form-label'
+      }, 'Zip'),
+      t('input', {
+        type: 'text',
+        class: 'form-control',
+        id: 'inputZip'
+      })
+    ]),
+    t('div', {
+      class: 'col-12'
+    }, [
+      t('div', {
+        class: 'form-check'
+      }, [
+        t('input', {
+          class: 'form-check-input',
+          type: 'checkbox',
+          id: "gridCheck"
+        }),
+        t('label', {
+          class: 'form-check-label',
+          for: 'gridCheck'
+        }, 'Check me out')
+      ])
+    ]),
+    t('div', {
+      class: 'col-12'
+    }, [
+      t('button', {
+        type: 'submit',
+        class: ['btn btn-primary']
+      }, 'Sign in')
+    ])
+  ]), `<form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4"/>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input
+      type="password"
+      class="form-control"
+      id="inputPassword4"
+    />
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input
+      type="text"
+      class="form-control"
+      id="inputAddress"
+      placeholder="1234 Main St"
+    />
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Address 2</label>
+    <input
+      type="text"
+      class="form-control"
+      id="inputAddress2"
+      placeholder="Apartment, studio, or floor"
+    />
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity"/>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="gridCheck"
+      />
+      <label class="form-check-label" for="gridCheck">Check me out</label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>`)
+})
+
+QUnit.test("define tags", function (assert) {
+  const tag = t
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, 'google!'), `<a href="www.google.com">google!</a>`)
+  assert.equal(t('super', {
+    href: 'www.google.com'
+  }, 'google!'), `<super href="www.google.com">google!</super>`)
+  assert.equal(t('hyper', {
+    href: 'www.google.com'
+  }, 'google!'), `<hyper href="www.google.com">google!</hyper>`)
+  assert.equal(t('deep', {
+    n: 5,
+    tag: "div"
+  }, 'Hello'), `<deep n="5" tag="div">Hello</deep>`)
+
+  t = t('super', function (tag, attr, html) {
+    return t('div', {
+      class: 'super'
+    }, [
+      t('a', attr, html)
+    ])
+  })
+
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, 'google!'), `<a href="www.google.com">google!</a>`)
+  assert.equal(t('super', {
+    href: 'www.google.com'
+  }, 'google!'), `<div class="super">
+  <a href="www.google.com">google!</a>
+</div>`)
+  assert.equal(t('hyper', {
+    href: 'www.google.com'
+  }, 'google!'), `<hyper href="www.google.com">google!</hyper>`)
+  assert.equal(t('deep', {
+    n: 5,
+    tag: "div"
+  }, 'Hello'), `<deep n="5" tag="div">Hello</deep>`)
+
+  t = t('hyper', function (tag, attr, html) {
+    return t('div', {
+      class: 'hyper'
+    }, [
+      t('super', attr, html)
+    ])
+  })
+
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, 'google!'), `<a href="www.google.com">google!</a>`)
+  assert.equal(t('super', {
+    href: 'www.google.com'
+  }, 'google!'), `<div class="super">
+  <a href="www.google.com">google!</a>
+</div>`)
+  assert.equal(t('hyper', {
+    href: 'www.google.com'
+  }, 'google!'), `<div class="hyper">
+  <div class="super">
+    <a href="www.google.com">google!</a>
+  </div>
+</div>`)
+  assert.equal(t('deep', {
+    n: 5,
+    tag: "div"
+  }, 'Hello'), `<deep n="5" tag="div">Hello</deep>`)
+
+  t = tag('deep', function (tag, attrs, html) {
+    if (attrs.n) {
+      attrs.n = parseInt(attrs.n) - 1
+      return t(attrs.tag || 'span', {}, [
+        t('deep', attrs, html)
+      ])
+    } else {
+      return t(attrs.tag || 'span', {}, html)
+    }
+  })
+
+  assert.equal(t('a', {
+    href: 'www.google.com'
+  }, 'google!'), `<a href="www.google.com">google!</a>`)
+  assert.equal(t('super', {
+    href: 'www.google.com'
+  }, 'google!'), `<super href="www.google.com">google!</super>`)
+  assert.equal(t('hyper', {
+    href: 'www.google.com'
+  }, 'google!'), `<hyper href="www.google.com">google!</hyper>`)
+  assert.equal(t('deep', {
+    n: 5,
+    tag: "div"
+  }, 'Hello'), `<div>
+  <div>
+    <div>
+      <div>
+        <div>
+          <div>Hello</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`)
+})
